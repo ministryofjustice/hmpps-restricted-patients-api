85a86b02747d60e8f4d3c6a0afea981b2508c973
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-06-24 10:17:13 +0100

    NN-3463: - Change API timeout value - Add in jwt public key defaults - Add client tracking

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,9 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.apache.commons:commons-text:1.9")
 
+  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
 
