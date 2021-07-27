79f9f8b17592638fdb6420dbb1a303c8a8eb1d2a
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-07-27 14:10:46 +0100

    NN-3465: - correct script name

--- src/test/resources/localstack/setup-queue.sh
+++ src/test/resources/localstack/setup-queue.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 export TERM=ansi
+export AWS_ACCESS_KEY_ID=foobar
+export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 
 aws --endpoint-url=http://localhost:4566 sns create-topic --name offender_events
