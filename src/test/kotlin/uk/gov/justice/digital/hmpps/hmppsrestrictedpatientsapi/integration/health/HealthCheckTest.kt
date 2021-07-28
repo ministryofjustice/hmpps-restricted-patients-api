8f06727998ec233d0a61e90c750f8823eb259258
Jamie Draper <draper65@googlemail.com>
2021-07-28 11:21:40 +0100

    NN-3465: part 1 (#16)
    * NN-3465:
    - add in queue integration
    
    * NN-3465:
    - update circle to run the local stack container
    
    * NN-3465:
    - update circle to run the local stack container
    
    * NN-3465:
    - fix formatting
    
    * NN-3465:
    - correct script name
    
    * NN-3465:
    - correct script name
    
    * NN-3465:
    - correct script name
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    * NN-3465
    
    Co-authored-by: JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/integration/health/HealthCheckTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/integration/health/HealthCheckTest.kt
@@ -1,8 +1,23 @@
 package uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.integration.health
 
+import com.amazonaws.services.sqs.AmazonSQS
+import com.amazonaws.services.sqs.model.GetQueueAttributesRequest
+import com.amazonaws.services.sqs.model.GetQueueAttributesResult
+import com.amazonaws.services.sqs.model.QueueAttributeName
+import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.mock.mockito.SpyBean
+import org.springframework.test.util.ReflectionTestUtils
+import uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.config.DlqStatus
+import uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.config.QueueAttributes
+import uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.config.QueueHealth
 import uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.integration.IntegrationTestBase
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -10,11 +25,128 @@ import java.util.function.Consumer
 
 class HealthCheckTest : IntegrationTestBase() {
 
+  @SpyBean
+  @Qualifier("awsSqsClientForOffenderEvents")
+  protected lateinit var awsSqsClient: AmazonSQS
+
+  @Autowired
+  private lateinit var queueHealth: QueueHealth
+
+  @Autowired
+  @Value("\${offender-events-sqs.queue.name}")
+  private lateinit var offenderEventsQueueName: String
+
+  @Autowired
+  @Value("\${offender-events-sqs.dlq.name}")
+  private lateinit var offenderEventsDlqName: String
+
   @BeforeEach
   fun beforeEach() {
     prisonApiMockServer.resetMappings()
     prisonApiMockServer.stubHealth()
     prisonerSearchApiMockServer.stubHealth()
+  }
+
+  @AfterEach
+  fun tearDown() {
+    ReflectionTestUtils.setField(queueHealth, "queueName", offenderEventsQueueName)
+    ReflectionTestUtils.setField(queueHealth, "dlqName", offenderEventsDlqName)
+  }
+
+  @Nested
+  inner class OffenderEventQueueTests {
+    @Test
+    fun `Queue does not exist reports down`() {
+      ReflectionTestUtils.setField(queueHealth, "queueName", "missing_queue")
+
+      webTestClient.get()
+        .uri("/health")
+        .headers(setHeaders())
+        .exchange()
+        .expectStatus().is5xxServerError
+        .expectBody()
+        .jsonPath("$.status").isEqualTo("DOWN")
+        .jsonPath("$.components.queueHealth.status").isEqualTo("DOWN")
+    }
+
+    @Test
+    fun `Queue health ok and dlq health ok, reports everything up`() {
+      webTestClient.get()
+        .uri("/health")
+        .headers(setHeaders())
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("$.status").isEqualTo("UP")
+        .jsonPath("$.components.queueHealth.status").isEqualTo("UP")
+        .jsonPath("$.components.queueHealth.details.dlqStatus").isEqualTo(DlqStatus.UP.description)
+    }
+
+    @Test
+    fun `Dlq health reports interesting attributes`() {
+      webTestClient.get()
+        .uri("/health")
+        .headers(setHeaders())
+        .exchange()
+        .expectBody()
+        .jsonPath("$.components.queueHealth.details.${QueueAttributes.MESSAGES_ON_DLQ.healthName}").isEqualTo(0)
+    }
+
+    @Test
+    fun `Dlq down brings main health and queue health down`() {
+      mockQueueWithoutRedrivePolicyAttributes()
+
+      webTestClient.get()
+        .uri("/health")
+        .headers(setHeaders())
+        .exchange()
+        .expectStatus().is5xxServerError
+        .expectBody()
+        .jsonPath("$.status").isEqualTo("DOWN")
+        .jsonPath("$.components.queueHealth.status").isEqualTo("DOWN")
+        .jsonPath("$.components.queueHealth.details.dlqStatus").isEqualTo(DlqStatus.NOT_ATTACHED.description)
+    }
+
+    @Test
+    fun `Main queue has no redrive policy reports dlq down`() {
+      mockQueueWithoutRedrivePolicyAttributes()
+
+      webTestClient.get()
+        .uri("/health")
+        .headers(setHeaders())
+        .exchange()
+        .expectStatus().is5xxServerError
+        .expectBody()
+        .jsonPath("$.components.queueHealth.details.dlqStatus").isEqualTo(DlqStatus.NOT_ATTACHED.description)
+    }
+
+    @Test
+    fun `Dlq not found reports dlq down`() {
+      ReflectionTestUtils.setField(queueHealth, "dlqName", "missing_queue")
+
+      webTestClient.get()
+        .uri("/health")
+        .headers(setHeaders())
+        .exchange()
+        .expectStatus().is5xxServerError
+        .expectBody()
+        .jsonPath("$.components.queueHealth.details.dlqStatus").isEqualTo(DlqStatus.NOT_FOUND.description)
+    }
+
+    private fun mockQueueWithoutRedrivePolicyAttributes() {
+      val queueName = ReflectionTestUtils.getField(queueHealth, "queueName") as String
+      val queueUrl = awsSqsClient.getQueueUrl(queueName)
+      whenever(
+        awsSqsClient.getQueueAttributes(
+          GetQueueAttributesRequest(queueUrl.queueUrl).withAttributeNames(
+            listOf(
+              QueueAttributeName.All.toString()
+            )
+          )
+        )
+      )
+        .thenReturn(GetQueueAttributesResult())
+    }
   }
 
   @Test
