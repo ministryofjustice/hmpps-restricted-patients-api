c4443475b616472cb7ddb389b985f8f967b152e6
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-08-05 09:25:03 +0100

    NN-3589: - tweak order of filter

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/config/UserContextFilter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/config/UserContextFilter.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.config
 
 import UserContext
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import java.io.IOException
@@ -13,6 +14,7 @@ import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
 
 @Component
+@Order(4)
 class UserContextFilter : Filter {
   @Throws(IOException::class, ServletException::class)
   override fun doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain) {
