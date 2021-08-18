5a937a2e1da3298e12cb4a04b80f931077cb7731
Jamie Draper <draper65@googlemail.com>
2021-08-18 10:56:43 +0100

    NN-3612 bump packages (#35)
    Co-authored-by: JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.6"
   kotlin("plugin.spring") version "1.5.20"
   kotlin("plugin.jpa") version "1.5.20"
 }
@@ -18,18 +18,18 @@ dependencies {
   implementation("io.springfox:springfox-boot-starter:3.0.0")
   implementation("org.springframework:spring-jms")
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.7")
   implementation("org.awaitility:awaitility-kotlin:4.1.0")
 
   runtimeOnly("com.h2database:h2")
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.postgresql:postgresql")
 
-  testImplementation("org.flywaydb:flyway-core:7.7.2")
+  testImplementation("org.flywaydb:flyway-core:7.14.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.27.0")
 }
 
 allOpen {
