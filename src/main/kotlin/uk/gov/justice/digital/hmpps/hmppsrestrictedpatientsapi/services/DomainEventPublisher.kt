48c932088da8b7b2f36b965752337ae72a74a8e9
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-09-07 08:00:27 +0100

    NN-3588 add in domain publisher config

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/services/DomainEventPublisher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/services/DomainEventPublisher.kt
@@ -2,12 +2,8 @@ package uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.services
 
 import com.amazonaws.services.sns.AmazonSNS
 import com.google.gson.Gson
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate
 import org.springframework.cloud.aws.messaging.core.TopicMessageChannel
-import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 data class DomainEvent(
@@ -25,19 +21,12 @@ interface DomainEventPublisher {
   fun publishRestrictedPatientRemoved(prisonerNumber: String)
 }
 
-@Service
-@ConditionalOnProperty(name = ["domain-events-sns.provider"], havingValue = "localstack")
 class StubDomainEventPublisher : DomainEventPublisher {
   override fun publishRestrictedPatientRemoved(prisonerNumber: String) {}
 }
 
-@Service
-@ConditionalOnProperty(name = ["domain-events-sns.provider"], havingValue = "aws")
-class DomainEventPublisherImpl(
-  @Qualifier("awsSnsClientForDomainEvents") client: AmazonSNS,
-  @Value("\${domain-events-sns.topic.arn}") topicArn: String,
-  private val gson: Gson
-) : DomainEventPublisher {
+class DomainEventPublisherImpl(client: AmazonSNS, topicArn: String, private val gson: Gson) :
+  DomainEventPublisher {
 
   val topicTemplate = NotificationMessagingTemplate(client)
   val topicMessageChannel = TopicMessageChannel(client, topicArn)
