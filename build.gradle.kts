b084b096069a1f86da67841d1bbff8376c3d145b
sp-watson <77974320+sp-watson@users.noreply.github.com>
2022-01-19 11:13:50 +0000

    NN-3921: Address security warnings - update h2database and related DB dependencies (#59)
    * NN-3921: Upgrade h2 to address security issues
    
    As per whereabouts-api, we have to update flyway and add a specific Postgres dialect to make it work
    
    * NN-3921: Remove test-only dependency for flyway
    
    * NN-3921: Reinstate test-only dependency for flyway
    
    For some reason it fails to compile tests via gradle (it's OK in the IDE)
    
    Co-authored-by: sp-watson <steve.watson@digital,justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,11 @@ dependencies {
   implementation("com.amazonaws:aws-java-sdk-sns:1.12.96")
   implementation("org.springframework.cloud:spring-cloud-aws-messaging:2.2.6.RELEASE")
 
-  runtimeOnly("com.h2database:h2")
-  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("com.h2database:h2:2.0.206")
+  runtimeOnly("org.flywaydb:flyway-core:8.4.1")
   runtimeOnly("org.postgresql:postgresql")
 
-  testImplementation("org.flywaydb:flyway-core:8.0.2")
+  testImplementation("org.flywaydb:flyway-core:8.4.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.springframework.security:spring-security-test")
