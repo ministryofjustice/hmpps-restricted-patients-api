a9ce1f08b8b11ea5cb20d23cca1bd7760282b982
sp-watson <steve.watson@digital,justice.gov.uk>
2022-01-19 10:22:15 +0000

    NN-3921: Reinstate test-only dependency for flyway
    For some reason it fails to compile tests via gradle (it's OK in the IDE)

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
   runtimeOnly("org.flywaydb:flyway-core:8.4.1")
   runtimeOnly("org.postgresql:postgresql")
 
+  testImplementation("org.flywaydb:flyway-core:8.4.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.springframework.security:spring-security-test")
