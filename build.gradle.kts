ca43016df3fe5c03429e233938a8447784949dcc
sp-watson <77974320+sp-watson@users.noreply.github.com>
2022-03-04 12:25:19 +0000

    NN-3963: Update dependencies, esp gradle-spring-boot (#68)
    Co-authored-by: sp-watson <steve.watson@digital,justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.3-beta"
-  kotlin("plugin.spring") version "1.5.30"
-  kotlin("plugin.jpa") version "1.5.30"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.5-beta"
+  kotlin("plugin.spring") version "1.6.10"
+  kotlin("plugin.jpa") version "1.6.10"
 }
 
 configurations {
@@ -17,21 +17,21 @@ dependencies {
   implementation("org.apache.commons:commons-text:1.9")
   implementation("io.springfox:springfox-boot-starter:3.0.0")
   implementation("org.springframework:spring-jms")
-  implementation("com.google.code.gson:gson:2.8.8")
+  implementation("com.google.code.gson:gson:2.9.0")
   implementation("org.awaitility:awaitility-kotlin:4.1.1")
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-  implementation("com.amazonaws:aws-java-sdk-sns:1.12.96")
+  implementation("com.amazonaws:aws-java-sdk-sns:1.12.171")
   implementation("org.springframework.cloud:spring-cloud-aws-messaging:2.2.6.RELEASE")
 
   runtimeOnly("com.h2database:h2:2.1.210")
-  runtimeOnly("org.flywaydb:flyway-core:8.4.1")
-  runtimeOnly("org.postgresql:postgresql:42.3.2")
+  runtimeOnly("org.flywaydb:flyway-core:8.5.1")
+  runtimeOnly("org.postgresql:postgresql:42.3.3")
 
-  testImplementation("org.flywaydb:flyway-core:8.4.1")
+  testImplementation("org.flywaydb:flyway-core:8.5.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.32.0")
 }
 
 allOpen {
