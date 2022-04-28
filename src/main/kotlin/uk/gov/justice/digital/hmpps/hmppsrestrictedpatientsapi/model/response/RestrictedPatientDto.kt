87d1f15c60f352baaa8b5982ad82934ee6c9269d
Tim Wright <wright.tim.j@gmail.com>
2022-04-28 13:10:15 +0100

    NN-3914 springdoc (#79)
    * springdoc
    
    * version 3 annotations

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/model/response/RestrictedPatientDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/model/response/RestrictedPatientDto.kt
@@ -1,28 +1,27 @@
 package uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.model.response
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import io.swagger.annotations.ApiModel
-import io.swagger.annotations.ApiModelProperty
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@ApiModel(description = "Restricted patient")
+@Schema(description = "Restricted patient")
 data class RestrictedPatientDto(
-  @ApiModelProperty(value = "Then NOMS Id of the restricted patient", example = "G1072GT")
+  @Schema(description = "Then NOMS Id of the restricted patient", example = "G1072GT")
   val prisonerNumber: String,
-  @ApiModelProperty(value = "Previous location id", example = "MDI")
+  @Schema(description = "Previous location id", example = "MDI")
   val fromLocation: Agency?,
-  @ApiModelProperty(value = "Hospital location code, this indicates the current location of a prisoner", example = "HAZLWD")
+  @Schema(description = "Hospital location code, this indicates the current location of a prisoner", example = "HAZLWD")
   val hospitalLocation: Agency?,
-  @ApiModelProperty(value = "Prison where the offender is supported by POM", example = "LEI")
+  @Schema(description = "Prison where the offender is supported by POM", example = "LEI")
   val supportingPrison: Agency? = null,
-  @ApiModelProperty(value = "Date and time the prisoner was discharged to hospital")
+  @Schema(description = "Date and time the prisoner was discharged to hospital")
   val dischargeTime: LocalDateTime,
-  @ApiModelProperty(value = "Useful comments")
+  @Schema(description = "Useful comments")
   val commentText: String? = null,
-  @ApiModelProperty(value = "Date time the record was created")
+  @Schema(description = "Date time the record was created")
   var createDateTime: LocalDateTime? = null,
-  @ApiModelProperty(value = "The user id of the person who created the record")
+  @Schema(description = "The user id of the person who created the record")
   var createUserId: String? = null
 )
 
