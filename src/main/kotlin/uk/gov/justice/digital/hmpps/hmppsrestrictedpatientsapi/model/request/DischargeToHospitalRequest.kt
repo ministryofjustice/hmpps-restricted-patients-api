96b769e1735d80f1e110bd00ed1903a906284e07
Mike Halma <mike.halma@digital.justice.gov.uk>
2022-10-12 16:38:05 +0100

    SDI-357 Use the internal service for discharge to hospital

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/model/request/DischargeToHospitalRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/model/request/DischargeToHospitalRequest.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.model.request
 
+import java.time.LocalDateTime
+
 data class DischargeToHospitalRequest(
   val offenderNo: String,
   val commentText: String? = null,
   val fromLocationId: String,
   val hospitalLocationCode: String,
-  val supportingPrisonId: String? = null
+  val supportingPrisonId: String? = null,
+  val dischargeTime: LocalDateTime? = null,
 )
