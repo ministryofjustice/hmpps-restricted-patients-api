f1a9365d1f7b201633816475c4738a6b60eeb206
petergphillips <peter@greenthistle.co.uk>
2022-11-15 17:01:08 +0000

    SDI-454: ⬆️  Upgrade to latest gradle spring boot for snakeyaml (#162)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.7"
-  kotlin("plugin.spring") version "1.7.20"
-  kotlin("plugin.jpa") version "1.7.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.6.0-beta"
+  kotlin("plugin.spring") version "1.7.21"
+  kotlin("plugin.jpa") version "1.7.21"
 }
 
 configurations {
@@ -15,13 +15,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.apache.commons:commons-text:1.10.0")
-  implementation("io.swagger:swagger-annotations:1.6.8")
+  implementation("io.swagger:swagger-annotations:1.6.9")
   implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
   implementation("org.springdoc:springdoc-openapi-data-rest:1.6.12")
   implementation("org.springframework:spring-jms")
   implementation("com.google.code.gson:gson:2.10")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.12")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.13")
   implementation("org.apache.commons:commons-csv:1.9.0")
 
   runtimeOnly("com.h2database:h2:2.1.214")
