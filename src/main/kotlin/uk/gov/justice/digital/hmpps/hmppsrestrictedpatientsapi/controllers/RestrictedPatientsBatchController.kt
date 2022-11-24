71c06891ef5f6b7cfea616f2ab2061947147762a
Mike Halma <mike.halma@digital.justice.gov.uk>
2022-11-24 11:38:23 +0000

    SDI-350 Use a better http method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/controllers/RestrictedPatientsBatchController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/controllers/RestrictedPatientsBatchController.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsrestrictedpatientsapi.controllers
 
 import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -16,7 +15,7 @@ class RestrictedPatientsBatchController(
   private val unknownPatientService: UnknownPatientService
 ) {
   @Hidden
-  @GetMapping(value = ["/process-past-date-restricted-patients"])
+  @PostMapping(value = ["/process-past-date-restricted-patients"])
   /**
    * Internal endpoint to find all restricted patients that have determinate sentences (non lifers) where the
    * conditional release date is in the past so shouldn't be handled by restricted patients anymore.
