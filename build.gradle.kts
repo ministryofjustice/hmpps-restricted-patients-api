a676d7cc6c035eb16c9f06578631a9d7ba49ae5f
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-10-28 20:33:44 +0000

    SDIT-2145: ⬆️ Use sqs lib 5.1.0 for better exception recording

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
   implementation("org.springframework:spring-jms")
   implementation("com.google.code.gson:gson:2.11.0")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.0.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.1.0")
   implementation("org.apache.commons:commons-csv:1.12.0")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
 
