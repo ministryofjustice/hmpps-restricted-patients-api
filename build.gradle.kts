2f89ce85ff1799c6e60ad8b9fafee963ab574844
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-03-04 08:14:49 +0000

    SDIT-2600 Try out latest Kotlin library (#344)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.0-beta")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
