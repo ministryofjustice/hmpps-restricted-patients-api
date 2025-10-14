ce1c2a5e5b331e1046728ab42fb7e9cbbdc7245a
petergphillips <5099053+petergphillips@users.noreply.github.com>
2025-10-14 10:07:42 +0100

    SDIT-3111: 🔒️ Upgrade to hmpps plugin 9.1.3 for owasp fix (#399)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.kotlin.dsl.register
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.3"
   kotlin("plugin.spring") version "2.2.20"
   kotlin("plugin.jpa") version "2.2.20"
 }
@@ -37,12 +37,12 @@ dependencies {
   testImplementation("org.flywaydb:flyway-core")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:4.1.1")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:5.0.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.34") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.35") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.38")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.39")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing:1.52.0")
 }
 
