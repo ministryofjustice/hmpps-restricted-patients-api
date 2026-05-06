39b2a84f929453f3aada617f4395ecb621ea1615
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-05-06 16:20:55 +0100

    SDIT-3675: ✨ Add in new dev-ui environment in the ui namespace (#447)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/services/OffenderEventSubscriber.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsrestrictedpatientsapi/services/OffenderEventSubscriber.kt
@@ -4,6 +4,7 @@ import com.google.gson.Gson
 import io.awspring.cloud.sqs.annotation.SqsListener
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 
 data class OffenderMovementReception(
@@ -14,6 +15,7 @@ data class OffenderMovementReception(
 data class Event(val Message: String)
 
 @Service
+@ConditionalOnProperty(name = ["application.subscribers.enabled"], havingValue = "true", matchIfMissing = true)
 class OffenderEventSubscriber(
   private val gson: Gson,
   private val restrictedPatientCleanup: RestrictedPatientCleanup,
